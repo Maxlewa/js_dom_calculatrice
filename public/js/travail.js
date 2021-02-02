b3ac269dd9003bccd41123c1e2a1296bc354f9c8
Maxence <lemairemaxence@hotmail.com>
2021-02-02 12:04:20 +0100

    progress

--- public/js/travail.js
+++ public/js/travail.js
@@ -1,3 +1,5 @@
+// CHIFFRES
+
 let neuf = document.querySelector('#neuf')
 let huit = document.querySelector('#huit')
 let sept = document.querySelector('#sept')
@@ -7,3 +9,33 @@ let quatre = document.querySelector('#quatre')
 let trois = document.querySelector('#trois')
 let deux = document.querySelector('#deux')
 let un = document.querySelector('#un')
+let zero = document.querySelector('#zero')
+
+let chiffre = document.querySelector('.chiffre')
+
+// OPERATIONS
+
+let plus = document.querySelector('#plus')
+let moins = document.querySelector('#moins')
+let fois = document.querySelector('#fois')
+let division = document.querySelector('#division')
+
+// CLEAR
+
+let clear = document.querySelector('#clear')
+
+// CALCULETTE 
+let calculette = document.querySelector('#calculette')
+
+// CLIQUER
+
+calculette.addEventListener('click', (e) => {
+    if (e.target.matches('button')) {
+        console.log(e.target.textContent);
+    }
+    
+})
+
+
+
+

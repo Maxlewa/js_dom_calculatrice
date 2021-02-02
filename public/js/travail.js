51bd1a908c1a9710138e8e1b0f745a5783c7dec2
Maxence <lemairemaxence@hotmail.com>
2021-02-02 12:34:19 +0100

    pause midi

--- public/js/travail.js
+++ public/js/travail.js
@@ -34,16 +34,36 @@ let ecran = document.querySelector('#ecran')
 
 // CLIQUER
 
+// calculette.addEventListener('click', (e) => {
+//     if (e.target.matches('button')) {
+//         console.log(e.target.textContent);
+//     }
+// })
+
+// calculette.addEventListener('click', (e) => {
+//     ecran.innerText = e.target.innerText
+// })
+
 calculette.addEventListener('click', (e) => {
-    if (e.target.matches('button')) {
-        console.log(e.target.textContent);
+    let key = e.target
+    let keyContent = key.textContent
+    let displayedNum = ecran.textContent
+    if (key.matches('button')) {
+        if (key.value === 'add' || key.value === 'subtract' || key.value === 'multiply' || key.value === 'divide') {
+            console.log(key.value);
+        } else if (key.value === 'calculate') {
+            console.log(key.value);
+        } else if (key.value === 'clear') {
+            console.log(key.value);
+        } else {
+            if (displayedNum === '0') {
+                ecranP.textContent = keyContent
+            } else {
+                ecranP.textContent = displayedNum + keyContent
+            }
+        }
     }
-})
-
-calculette.addEventListener('click', (e) => {
-    ecran.innerText = e.target.innerText
-})
-
+});
 
 
 
